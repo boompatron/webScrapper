313bc6d74fc5440c386a1a2df6ce33f0df79526a
boompatron <boompatron@naver.com>
2022-05-16 23:22:40 +0900

    220516_3_imported_requests_and_Beautiful_soup

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def scrapper():
-    print("this is scrapper")
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&limit=50")
+    indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
+    print(indeed_soup)
 
 
 if __name__ == "__main__":
-    scrapper()
+    main()
